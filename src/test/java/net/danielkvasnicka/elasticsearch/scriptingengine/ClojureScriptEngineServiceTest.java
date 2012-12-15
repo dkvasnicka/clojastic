c9a85557de1358955eaa0c6e22aace3d5a93bc16
Daniel Kvasnicka <daniel.kvasnicka@me.com>
2012-12-15 15:37:16 +0100

    Changed the test to run on snippet containing more forms

--- src/test/java/net/danielkvasnicka/elasticsearch/scriptingengine/ClojureScriptEngineServiceTest.java
+++ src/test/java/net/danielkvasnicka/elasticsearch/scriptingengine/ClojureScriptEngineServiceTest.java
@@ -27,7 +27,7 @@ public class ClojureScriptEngineServiceTest {
         put("b", 2);
     }};
     private static final String RUNNABLE_SCRIPT_TAKING_VAR_MAP =
-            "(ns test (:require [clojure.test :refer :all])) (defn x [m] (reduce + (filter number? (.values m))))";
+            "(ns test (:require [clojure.test :refer :all])) (set! *warn-on-reflection* true) (defn x [m] (reduce + (filter number? (.values m))))";
 
     private ClojureScriptEngineService service;
 
